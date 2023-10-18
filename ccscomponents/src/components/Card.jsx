968036d806b842503df78bc452e1d0f9dc8651e7
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-18 16:10:52 +0200

    ajout de la card

--- ccscomponents/src/components/Card.jsx
+++ ccscomponents/src/components/Card.jsx
@@ -1,48 +1,82 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Card = () => {
-
-    const Card = styled.figcaption`
-    background: 'grey';
+const Card = styled.figcaption`
+    background: black;
     border-radius: 10px;
-    `
-    const Top = styled.div
-    const RightTop = styled.div
+    width: 45%;
+    margin: auto;
+    color: white;
+`;
 
-    const Content = styled.div
-    const Title = styled.h1
-    const Name = styled.p
-    const Year = styled.p
+const Top = styled.div`
+display: grid;
+grid-template-columns: 60% 40%;
+`;
+const LeftTop = styled.div`
+`;
 
-    const Footer = styled.div
-    const Rate = styled.h2
-    const Ul = styled.ul
-    const Li = styled.li
+const Content = styled.div`
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+gap: 15px;
+text-align: center;
+`;
+const Title = styled.h1``;
+const Name = styled.p`
 
+font-size: 2rem;
+`;
+const Year = styled.p``;
+
+const Footer = styled.div`
+ display: flex;
+ justify-content: space-around;
+ padding-bottom:1rem;
+`;
+const Rate = styled.h2`
+display: flex;
+align-items: center;
+`;
+const Ul = styled.ul`
+display: flex;
+align-items: center;
+width: 15%;
+`;
+const Li = styled.li`
+list-style: none;
+`;
+
+const CardComponent = () => {
     return (
-        <Card className="container" >
-        <Top>
-        <RightTop>
-        <img src=""/>
-        </RightTop>
-        <Content>
-        <Title />
-        <Name>Under the Grave</Name>
-        <Year>(2016)</Year>
-        </Content>
-        </Top>
-        <Footer >
-        <Rate>Rate this album</Rate>
-        <Ul>
-        <Li><i className="fa-regular fa-star"></i></Li>
-        <Li><i className="fa-regular fa-star"></i></Li>
-        <Li><i className="fa-regular fa-star"></i></Li>
-        <Li><i className="fa-regular fa-star"></i></Li>
-        <Li><i className="fa-regular fa-star"></i></Li>
-        </Ul>
-        </Footer>
+        <Card className="container">
+            <Top>
+                <LeftTop>
+                <LeftTop>
+                <img style={{ width: '280px', height:'220px' }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmdbzjRwyUKIZ22emCqL0Ittx2qxLFe3ZgRQ&usqp=CAU" alt="album cover" />
+            </LeftTop>
+            
+                </LeftTop>
+                <Content>
+                    <Title>Manga Character</Title>
+                    <Name>Saitama</Name>
+                    <Year>(2019)</Year>
+                </Content>
+            </Top>
+            <Footer>
+                <Rate>Note ce heros</Rate>
+                <Ul>
+                    <Li><i className="fa-regular fa-star"></i></Li>
+                    <Li><i className="fa-regular fa-star"></i></Li>
+                    <Li><i className="fa-regular fa-star"></i></Li>
+                    <Li><i className="fa-regular fa-star"></i></Li>
+                    <Li><i className="fa-regular fa-star"></i></Li>
+                </Ul>
+            </Footer>
         </Card>
     );
 };
 
-export default Card;
+export default CardComponent;
